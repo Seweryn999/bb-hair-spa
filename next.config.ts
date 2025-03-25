bd87bbc21fc14a2846c55c370d6b4c44c52b7164
Seweryn <sewcio0212@gmail.com>
2025-03-25 17:47:18 +0100

    adjustments.2

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig = {
   // Włączamy statyczny eksport
   output: "export",
   // Ustawiamy basePath i assetPrefix na nazwę repozytorium (w produkcji)
-  basePath: isProd ? "/moja-strona" : "",
-  assetPrefix: isProd ? "/moja-strona/" : "",
+  basePath: isProd ? "/bb-hair-spa" : "",
+  assetPrefix: isProd ? "/bb-hair-spa/" : "",
   // Wyłączamy optymalizację obrazów w trybie statycznym
   images: {
     unoptimized: true,
