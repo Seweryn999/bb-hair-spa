eef4dafb54e7749127d78d76decf198619e259c9
Seweryn <sewcio0212@gmail.com>
2025-03-25 15:22:35 +0100

    config adjustment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/bb-hair-spa",
+  assetPrefix: "/bb-hair-spa/",
   devIndicators: false,
 };
 
