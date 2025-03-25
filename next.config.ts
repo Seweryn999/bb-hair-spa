dfb8f6df613abe4035537770f69462ddec494a7b
Seweryn <sewcio0212@gmail.com>
2025-03-25 18:15:17 +0100

    adjustments.3

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,9 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  // Włączamy statyczny eksport
   output: "export",
-  // Ustawiamy basePath i assetPrefix na nazwę repozytorium (w produkcji)
   basePath: isProd ? "/bb-hair-spa" : "",
-  assetPrefix: isProd ? "/bb-hair-spa/" : "",
-  // Wyłączamy optymalizację obrazów w trybie statycznym
+  // assetPrefix: isProd ? "/bb-hair-spa/" : "",  // <-- Wykomentuj to na próbę
   images: {
     unoptimized: true,
   },
