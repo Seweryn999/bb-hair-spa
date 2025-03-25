fc35d347241b16cc64b16f7ffa732c5f3f162770
Seweryn <sewcio0212@gmail.com>
2025-03-25 22:23:05 +0100

    yml and config tweaks

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+// Zmodyfikuj environment tak, by zawsze mieć poprzedzający slash
+// Jeśli user wstawi "bb-hair-spa", to i tak dodamy "/" z przodu
+function ensureLeadingSlash(path: string) {
+  if (!path.startsWith("/")) {
+    return "/" + path;
+  }
+  return path;
+}
+
+let basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+basePath = ensureLeadingSlash(basePath);
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
