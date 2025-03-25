ca4f8413b2f407e2ac47e9bc149726156113d4b3
Seweryn <sewcio0212@gmail.com>
2025-03-25 21:41:05 +0100

    config adjustments

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  // Dodaj poniższe dwie linie, jeśli publikujesz w podkatalogu "bb-hair-spa"
+  basePath: "/bb-hair-spa",
+  assetPrefix: "/bb-hair-spa",
 };
 
 export default nextConfig;
