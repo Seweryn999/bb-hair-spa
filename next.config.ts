0a4cdf510d4f1d139f8c58036b28e8578fb73938
Seweryn <sewcio0212@gmail.com>
2025-03-25 17:36:00 +0100

    test deploy

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig: NextConfig = {
-  devIndicators: false,
+const nextConfig = {
+  // Włączamy statyczny eksport
   output: "export",
+  // Ustawiamy basePath i assetPrefix na nazwę repozytorium (w produkcji)
+  basePath: isProd ? "/moja-strona" : "",
+  assetPrefix: isProd ? "/moja-strona/" : "",
+  // Wyłączamy optymalizację obrazów w trybie statycznym
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
