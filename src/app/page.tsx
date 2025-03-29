3d49a688450ca9a4e51d91a704dd85a0aa1d4974
Seweryn <sewcio0212@gmail.com>
2025-03-29 09:28:12 +0100

    seo plugins installed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import HeroSection from "../components/sections/HeroSection";
 import AboutUs from "../components/sections/AboutUs";
 import Services from "../components/sections/Services";
